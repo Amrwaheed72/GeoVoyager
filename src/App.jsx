e9103629549a2185add2d33665fa7bd414624a7f
Amr Waheed <amrmohamed877@gmail.com>
2024-11-30 22:28:13 +0200

    implementing React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Product from './Pages/Product'
+import HomePage from './Pages/HomePage'
 import './App.css'
+import Pricing from './Pages/Pricing'
+import PageNotFount from './Pages/PageNotFount'
+
 
 function App() {
   return (
-    <>
-      <div>hello World</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='*' element={<PageNotFount />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
