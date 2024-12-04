126c8fb6b4c837aceff06f33b981f3eae4255675
Amr Waheed <amrmohamed877@gmail.com>
2024-12-04 22:01:38 +0200

    some updates

--- src/Components/CityItem.jsx
+++ src/Components/CityItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import styles from './CityItem.module.css'
+import { useCities } from '../contexts/CitiesProvider';
 
 function CityItem({ city }) {
     const formatDate = (date) =>
@@ -9,10 +10,14 @@ function CityItem({ city }) {
             year: "numeric",
             weekday: "long",
         }).format(new Date(date));
-    const { cityName, emoji, date, id,position } = city
+
+    const {currentCity} = useCities()
+    const { cityName, emoji, date, id, position } = city
     return (
         <li>
-            <Link to={`${id}?lat=${position.lat}&lng=${position.lng}`} className={styles.cityItem}>
+            <Link to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+                className={`${styles.cityItem} ${id === currentCity.id ?
+                    styles['cityItem--active'] : ''}`}>
                 <span className={styles.emoji}>{emoji}</span>
                 <h3 className={styles.name}>{cityName}</h3>
                 <time className={styles.date}>({formatDate(date)})</time>
