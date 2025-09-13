a449e649e6f8534ebd607da8f4896b1766f6db38
Amr Waheed <amrmohamed877@gmail.com>
2025-09-13 23:33:52 +0300

    ..

--- src/Pages/ProtectedRoute.jsx
+++ src/Pages/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "./contexts/AuthProvider";
+import { useAuth } from "../contexts/AuthProvider";
 import { useEffect } from "react";
 
 function ProtectedRoute({ children }) {
